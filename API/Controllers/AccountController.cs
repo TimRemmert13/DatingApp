08f145c455b7d52aadaa1fe3a799d22cbd136a07
Tim Remmert <tremmert93@gmail.com>
2021-01-18 09:58:38 -0500

    Implemented extended User API

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -30,7 +30,7 @@ namespace API.Controllers
 
       var user = new AppUser
       {
-        UserName = registerDto.Username.ToLower(),
+        Username = registerDto.Username.ToLower(),
         PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
         PasswordSalt = hmac.Key
       };
@@ -39,7 +39,7 @@ namespace API.Controllers
       await _context.SaveChangesAsync();
       return new UserDto
       {
-          Username = user.UserName,
+          Username = user.Username,
           Token = _tokenService.CreateToken(user)
       };
     }
@@ -47,7 +47,7 @@ namespace API.Controllers
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-      var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+      var user = await _context.Users.SingleOrDefaultAsync(x => x.Username == loginDto.Username);
 
       if (user == null) return Unauthorized("Invalid username");
 
@@ -62,14 +62,14 @@ namespace API.Controllers
 
       return new UserDto
       {
-          Username = user.UserName,
+          Username = user.Username,
           Token = _tokenService.CreateToken(user)
       };
     }
 
     private async Task<bool> UserExists(string username)
     {
-      return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+      return await _context.Users.AnyAsync(x => x.Username == username.ToLower());
     }
   }
 }
--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -10,35 +11,35 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services
 {
-  public class TokenService : ITokenService
-  {
-    private readonly SymmetricSecurityKey _key;
-    public TokenService(IConfiguration config)
+    public class TokenService : ITokenService
     {
-        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.Username)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
     }
-
-    public string CreateToken(AppUser user)
-    {
-      var claims = new List<Claim>
-      {
-          new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
-      };
-
-      var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
-
-      var tokenDescriptor = new SecurityTokenDescriptor
-      {
-          Subject = new ClaimsIdentity(claims),
-          Expires = DateTime.Now.AddDays(7),
-          SigningCredentials = creds
-      };
-
-      var tokenHandler = new JwtSecurityTokenHandler();
-
-      var token = tokenHandler.CreateToken(tokenDescriptor);
-
-      return tokenHandler.WriteToken(token);
-    }
-  }
 }
