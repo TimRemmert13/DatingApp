e67426b0c98eccc3b9463349ce4c8dffe024db4b
Tim Remmert <tremmert93@gmail.com>
2021-01-19 09:40:46 -0500

    implemented members details page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,11 +8,15 @@ import { AccountService } from './_services/account.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'The Dating app';
   users: any;
 
   constructor(private accountService: AccountService) {}
+
+  ngOnInit(): void {
+    this.setCurrentUser();
+  }
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
